6b9f133fc894bf29fffc937715979dbc229740dc
Xx-Diego-XX-Agustin-xx-Garcia-xX <116301540+Xx-Diego-XX-Agustin-xx-Garcia-xX@users.noreply.github.com>
2024-10-16 11:28:09 -0700

    Update a123_apple_1.py
    Updated a123_apple_1.py, giving it a new function that causes an apple to fall.

--- a123_apple_1.py
+++ a123_apple_1.py
@@ -2,12 +2,12 @@
 import turtle as trtl
 
 #setup
-apple_image = "apple.gif" # Store the file name of your shape
-
+apple_image = "pear.gif"
+background_image = "background.gif"
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
-wn.addshape(apple_image) # Make the screen aware of the new file
-
+wn.addshape(apple_image)
+wn.bgpic(background_image)
 apple = trtl.Turtle()
 
 #functions
@@ -15,9 +15,12 @@ apple = trtl.Turtle()
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
-
-
+#given a turtle, set that turtle to fall to the ground
+def apple_fall(active_apple):
+  xcor = active_apple.xcor()
+  ycor = active_apple.ycor()
+  active_apple.goto(xcor, ycor - 50)
 #function calls
-draw_apple(apple)
+draw_apple(apple_image)
 
 wn.mainloop()
